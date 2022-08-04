3a32ad428e3e86ba89985b1a6a44e10f9cd2a22b
xfnw <xfnw@riseup.net>
2022-08-04 15:28:03 -0400

    it can read!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use serde::Deserialize;
 use serenity;
 use serenity::model::prelude::MessageId;
 use mcping;
-use toml;
+use toml::from_str;
+use std::fs::read_to_string;
+use std::process::exit;
 
 
 #[derive(Deserialize)]
@@ -16,5 +18,24 @@ struct Config {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let filename = "discst.toml";
+    println!("Hello, {}!",filename);
+
+    let contents = match read_to_string(filename) {
+        Ok(h) => h,
+        Err(_a) => {
+            eprintln!("failed to read {} ut oh", filename);
+            exit(69);
+        }
+    };
+
+    let config: Config = match from_str(&contents) {
+        Ok(h) => h,
+        Err(_a) => {
+            eprintln!("failed to parse {}. did you put all the required options?", filename);
+            exit(69);
+        }
+    };
+
+    println!("the heccin port is {} lol", config.port.unwrap_or(25565));
 }
